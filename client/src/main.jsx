4f28faa05708a8af29c5637ad65e1a33e11a5685
ARYANANDHA <aryanandhaaryanandha5@gmail.com>
2024-10-26 18:40:55 +0530

    Deploy client and server

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// src/index.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';  // Tailwind CSS styles
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { CartProvider } from './hooks/useCart';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+    <CartProvider>
+      <App />
+      </CartProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
